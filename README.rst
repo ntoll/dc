76cd67be40653c7e5d27018e49ee4c4406750192
Nicholas H.Tollervey <ntoll@ntoll.org>
2014-12-04 14:23:13 +0000

    Cleaned the boiler plate then threw it away. ;-)

--- README.rst
+++ README.rst
@@ -1,3 +1,4 @@
 Data Catalogue loading utilities
+================================
 
 Expects a config file to be located at ~/.dc
--- setup.py
+++ setup.py
@@ -1,32 +1,19 @@
 import re
 import os
-
 from setuptools import setup
+from dc import __version__
 
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = open('README.md').read()
 
-
-VERSION_FILE = os.path.join(here, "dc/_version.py")
-verstrline = open(VERSION_FILE, "rt").read()
-VSRE = r'^__version__ = [\'"]([^\'"]*)[\'"]'
-mo = re.search(VSRE,  verstrline, re.M)
-if mo:
-    VERSION = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in {0}".format(VERSION_FILE))
-
-os.chdir(here)
 
 setup(
     name = "dc",
-    version = VERSION,
+    version = __version__,
     url = "https://github.com/davidmiller/dc",
     description = "Data Catalogue",
     long_description = README,
-    # install_requires = [
-    #     ],
+    install_requires = ['ffs', ],
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
